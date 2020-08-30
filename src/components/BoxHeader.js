b226f21019d9dfd5a552c6d26a476a69c4dff46b
Phat <you@example.com>
2020-08-30 23:32:35 +0700

    add BoxHeader and LatestAlbum 2020 components

---
+++ src/components/BoxHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './BoxHeader.css';
+
+export default function BoxHeader() {
+    return (
+        <div className = "BoxHeader">
+            <a className = "ViewAll">Album mới nhất 2020</a>
+            <a className = "LinkMore">Xem tất cả</a>
+        </div>
+    )
+}
