33f528fe288821785ef5834f13e26570e6362e11
Phat <you@example.com>
2020-08-31 23:56:33 +0700

    Beautify file

--- src/components/BoxHeader.js
+++ src/components/BoxHeader.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './BoxHeader.css';
 
-export default function BoxHeader() {
+export default function(props) {
     return (
-        <div className = "BoxHeader">
-            <a className = "ViewAll">Album mới nhất 2020</a>
-            <a className = "LinkMore">Xem tất cả</a>
+        <div className="BoxHeader">
+            <a className="ViewAll">{props.title}</a>
+            <a className="LinkMore">Xem tất cả</a>
         </div>
     )
 }
