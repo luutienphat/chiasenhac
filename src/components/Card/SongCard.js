a0e97ffb2500f31ccfbeab158d7f4d9c29bdf484
Phat <you@example.com>
2020-09-01 00:04:14 +0700

    create SongCard component and its css

---
+++ src/components/Card/SongCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SongCard.css';
+
+export default function(){
+    return (
+            <div className = "SongCard">
+                    <div className = "ImageCover"></div>
+                    <div className = "SongInformation"></div>
+                    <div className = "SongTracking"></div>
+            </div>
+    )
+}
